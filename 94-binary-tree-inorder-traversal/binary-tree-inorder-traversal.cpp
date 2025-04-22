41bcb067e18a5c85c19c8627c3f086a0fa567470
Nagendra Marisetti <61658750+Nag28endra@users.noreply.github.com>
2025-04-22 07:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (66.33%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,18 +11,41 @@
  */
 class Solution {
 public:
-    void inOrder(TreeNode* root,vector<int> &res){
-        if (root == NULL)return;
-        // traverse to left
-        inOrder(root->left,res);
-        // print the value
-        res.push_back(root->val);
-        // traverse right
-        inOrder(root->right,res);
-    }
+    // USING RECURSSION
+    // void inOrder(TreeNode* root,vector<int> &res){
+    //     if (root == NULL)return;
+    //     // traverse to left
+    //     inOrder(root->left,res);
+    //     // print the value
+    //     res.push_back(root->val);
+    //     // traverse right
+    //     inOrder(root->right,res);
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
-        inOrder(root,res);
+        stack<TreeNode*> st;
+        // take the root node
+        TreeNode* node = root;
+
+        while(true){
+            // move to left if the node is not null
+            if (node != NULL){
+                st.push(node);
+                // move to the left
+                node = node->left;
+            }
+            // move to right if the current node is null - backtracking.
+            else{
+                if (st.empty()) break;
+                // take the top of the node.
+                node = st.top();
+                st.pop();
+                // add the value to our result.
+                res.push_back(node->val);
+                // go to the right of the current node.
+                node = node->right;
+            }
+        }
         return res;
     }
 };
