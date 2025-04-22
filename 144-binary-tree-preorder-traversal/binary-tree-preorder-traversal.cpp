707606cd88326bfb3fb06da4d8d377ff93fc921d
Nagendra Marisetti <61658750+Nag28endra@users.noreply.github.com>
2025-04-22 06:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (43.58%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,15 +11,30 @@
  */
 class Solution {
 public:
-    void preOrder(vector<int>& res, TreeNode* root){
-        if (root == NULL) return;
-        res.push_back(root->val);
-        preOrder(res,root->left);
-        preOrder(res,root->right);
-    }
+    // USING RECURSION
+    // void preOrder(vector<int>& res, TreeNode* root){
+    //     if (root == NULL) return;
+    //     res.push_back(root->val);
+    //     preOrder(res,root->left);
+    //     preOrder(res,root->right);
+    // }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> res;
-        preOrder(res,root);
+        if (root == NULL) return res;
+        // to trace the nodes of the tree
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            // take out of the top node of the stack
+            TreeNode* node = st.top();
+            st.pop();
+            // check whether right and left nodes are present or not.
+            if (node->right !=NULL) st.push(node->right);
+            if (node->left != NULL) st.push(node->left);
+            // add the top of the stack node value to the result which was just popped.
+            res.push_back(node->val);
+        }
         return res;
     }
 };
