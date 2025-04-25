25e2afaad553ef0f74280b31365c39b89d65c48a
Nagendra Marisetti <61658750+Nag28endra@users.noreply.github.com>
2025-04-25 18:54:40 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (44.44%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -11,16 +11,34 @@
  */
 class Solution {
 public:
-    void postOrder(TreeNode* root, vector<int> &res){
-        if (root == NULL)return;
+    // Using RECURSION
+    // void postOrder(TreeNode* root, vector<int> &res){
+    //     if (root == NULL)return;
 
-        postOrder(root->left, res);
-        postOrder(root->right,res);
-        res.push_back(root->val);
-    }
+    //     postOrder(root->left, res);
+    //     postOrder(root->right,res);
+    //     res.push_back(root->val);
+    // }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> res;
-        postOrder(root, res);
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+        // traverse the first stack to keep track of nodes.
+        while(!st1.empty()){
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if (root == NULL) return res;
+            // check the left and right of the current node.
+            if (root->left) st1.push(root->left);
+            if (root->right) st1.push(root->right);
+
+        }
+        // at last pop the top values and store them in our result.
+            while(!st2.empty()){
+                res.push_back(st2.top()->val);
+                st2.pop();
+            }
         return res;
     }
 };
