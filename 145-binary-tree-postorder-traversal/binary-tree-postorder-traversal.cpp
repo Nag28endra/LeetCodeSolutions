aa5b20c6761bc831f33e203bc6fa187a63b99290
Nagendra Marisetti <61658750+Nag28endra@users.noreply.github.com>
2025-04-25 20:19:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (92.33%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -19,26 +19,62 @@ public:
     //     postOrder(root->right,res);
     //     res.push_back(root->val);
     // }
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> res;
-        stack<TreeNode*> st1,st2;
-        st1.push(root);
-        // traverse the first stack to keep track of nodes.
-        while(!st1.empty()){
-            root = st1.top();
-            st1.pop();
-            st2.push(root);
-            if (root == NULL) return res;
-            // check the left and right of the current node.
-            if (root->left) st1.push(root->left);
-            if (root->right) st1.push(root->right);
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     vector<int> res;
+    //     stack<TreeNode*> st1,st2;
+    //     st1.push(root);
+    //     // traverse the first stack to keep track of nodes.
+    //     while(!st1.empty()){
+    //         root = st1.top();
+    //         st1.pop();
+    //         st2.push(root);
+    //         if (root == NULL) return res;
+    //         // check the left and right of the current node.
+    //         if (root->left) st1.push(root->left);
+    //         if (root->right) st1.push(root->right);
 
-        }
-        // at last pop the top values and store them in our result.
-            while(!st2.empty()){
-                res.push_back(st2.top()->val);
-                st2.pop();
+    //     }
+    //     // at last pop the top values and store them in our result.
+    //         while(!st2.empty()){
+    //             res.push_back(st2.top()->val);
+    //             st2.pop();
+    //         }
+    //     return res;
+    // }
+
+     vector<int> postorderTraversal(TreeNode* root){
+        vector<int> res;
+        stack<TreeNode*> st;
+        // define a current node.
+        TreeNode* currentNode = root;
+        while(currentNode != NULL || !st.empty()){
+            // move to the left of the node if the current node is not null
+            if (currentNode != NULL){
+                st.push(currentNode);
+                currentNode = currentNode->left;
             }
+            // if the current node is null
+            else{
+                // move to the right of the current nonde.
+                TreeNode* temp = st.top()->right;
+                // if that right node is not null make it as current node and move towards right.
+                if (temp!=NULL) currentNode = temp;
+                // if the right node is null 
+                else{
+                    // pick the top of the stack 
+                    temp = st.top();
+                    st.pop();
+                    // add it to the result.
+                    res.push_back(temp->val);
+                    // if the stack is not empty and there is any root for the right node and add them to the result
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        res.push_back(temp->val);
+                    }
+                }
+            }
+        }
         return res;
-    }
+     }
 };
